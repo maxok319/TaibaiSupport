034d8e50733cd146f066e4e652efd1a3393e3c13
wangxinkuan <wangxinkuan@mmears.com>
2019-05-11 23:55:51 +0800

    用闭包的方式提供一个messageId生成器

---
+++ TaibaiUtils/TaibaiUtils.go
@@ -0,0 +1,39 @@
+package TaibaiUtils
+
+import (
+	"strconv"
+	"sync"
+	"time"
+)
+
+// 切勿直接使用这个
+func generateMessageId() func()int {
+
+	var GenerateMessageMutex sync.Mutex
+	var LastMessageTimeS	int
+	var LastMessageIndex	int
+
+	return func() int {
+		// ("2006-01-02 15:04:05.999999999 -0700 MST")
+		// 年月日十分秒*1万 + 此秒内的index
+		GenerateMessageMutex.Lock()
+		defer GenerateMessageMutex.Unlock()
+
+		loc, _:= time.LoadLocation("Asia/Chongqing")
+		beijingTimeStr := time.Now().In(loc).Format("20060102150405")
+		beijingTime,_ := strconv.Atoi(beijingTimeStr)
+		if beijingTime!=LastMessageTimeS {
+			LastMessageIndex = 1
+		} else {
+			LastMessageIndex = LastMessageIndex + 1
+		}
+
+		return beijingTime * 10000 + LastMessageIndex
+	}
+
+}
+
+var GenerateMessageId func()int
+func init() {
+	GenerateMessageId = generateMessageId()
+}
