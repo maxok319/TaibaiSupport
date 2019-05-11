5f8db175dca5f888a260a7bd31107d1ec3ff6116
wangxinkuan <wangxinkuan@mmears.com>
2019-05-11 15:47:55 +0800

    删除PythonClient，为将其作为submodule作准备

--- PythonClient/TaibaiConfig.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-
-taibai_config = {
-    "standard_coursearea_size": QSize(1440, 810),
-    "standard_coursearea_width" : 1440,
-    "standard_coursearea_height" : 810
-}
