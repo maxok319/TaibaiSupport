67f95817026db835effb17bf703bd998653a602a
wangxinkuan <wangxinkuan@mmears.com>
2019-05-21 02:15:09 +0800

    docker-compose一件发布完成

---
+++ TaibaiProduct.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# define env var default value.
+export rabbitmq_addr=host.docker.internal
+export rabbitmq_user=taibai-support
+export rabbitmq_passwd=taibai-support
+export classroom_region=1
+
+docker-compose build --build-arg branch_name=master
+docker-compose up
