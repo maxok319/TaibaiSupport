1691292c745f82543db133fcb6b7fc0c84f98383
wangxinkuan <wangxinkuan@mmears.com>
2019-05-21 20:56:59 +0800

    用registration来自动注册服务

--- TaibaiBeta.sh
+++ TaibaiBeta.sh
@@ -3,7 +3,9 @@
 export rabbitmq_addr=host.docker.internal
 export rabbitmq_user=taibai-support
 export rabbitmq_passwd=taibai-support
-export classroom_region=1
+export classroom_region=3
+export consul_addr=10.30.48.16
+export host_addr=10.30.48.16
 
 docker-compose down
 docker-compose build --build-arg branch_name=beta
