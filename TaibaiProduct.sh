5936b3a4a7d843d141e63f459779ca234db75160
wangxinkuan <wangxinkuan@mmears.com>
2019-05-22 02:57:08 +0800

    consul client和服务注册

--- TaibaiProduct.sh
+++ TaibaiProduct.sh
@@ -3,9 +3,9 @@
 export rabbitmq_addr=host.docker.internal
 export rabbitmq_user=taibai-support
 export rabbitmq_passwd=taibai-support
-export classroom_region=3
-export consul_addr=10.30.48.16
-export host_addr=10.30.48.16
+export classroom_region=
+export consul_server_addr=
+export host_addr=
 
 docker-compose down
 docker-compose build --build-arg branch_name=master
